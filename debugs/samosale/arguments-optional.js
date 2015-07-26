f126ee4746cf1ed997b8223ef4d197f536641925
Nathan Gray <njgray@ualr.edu>
2015-07-26 15:55:56 -0500

    Update arguments-optional.js
    Corrected the above problem, but still missing a conditional step to solve the bonfire

--- debugs/samosale/arguments-optional.js
+++ debugs/samosale/arguments-optional.js
@@ -7,14 +7,12 @@ function add(arg1, arg2) {
     }
   
   if (typeof(arg1) === 'number' && typeof(arg2) === 'number') {
-  
       if (arguments.length === 2) {
           return arg1+arg2;
-      } else {
-        return sum2and;
-        
-      }
+      } 
+  } else {
+    return sum2and;
   }
-
+  
   return undefined;
 }
