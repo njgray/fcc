42a12d218bc10c3b2cc9c673b6eecfc370899fb3
Nathan Gray <njgray@ualr.edu>
2015-07-26 15:55:24 -0500

    Create arguments-optional.js
    Doesn't return properly

---
+++ debugs/samosale/arguments-optional.js
@@ -0,0 +1,20 @@
+function add(arg1, arg2) {
+
+   function sum2and(num) {
+      if (typeof arg1 === 'number') {
+        return num + arg1;
+      } 
+    }
+  
+  if (typeof(arg1) === 'number' && typeof(arg2) === 'number') {
+  
+      if (arguments.length === 2) {
+          return arg1+arg2;
+      } else {
+        return sum2and;
+        
+      }
+  }
+
+  return undefined;
+}
